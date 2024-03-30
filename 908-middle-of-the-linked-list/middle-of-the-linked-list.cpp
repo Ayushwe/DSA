212fd3c710b5b7ecad295591906ebd4b43a345ef
Ayushwe <114506169+Ayushwe@users.noreply.github.com>
2024-03-30 16:25:31 +0530

    Time: 0 ms (100.00%) | Memory: 8.5 MB (98.72%) - LeetSync

--- 908-middle-of-the-linked-list/middle-of-the-linked-list.cpp
+++ 908-middle-of-the-linked-list/middle-of-the-linked-list.cpp
@@ -10,27 +10,34 @@
  */
 class Solution {
 public:
-    int getLength(ListNode* head) {
-        int count = 0;
-        while (head != nullptr) {
-            count++;
-            head = head->next;
-        }
-        return count;
-    }
+    // int getLength(ListNode* head) {
+    //     int count = 0;
+    //     while (head != nullptr) {
+    //         count++;
+    //         head = head->next;
+    //     }
+    //     return count;
+    // }
 
     ListNode* middleNode(ListNode* head) {
         if (head == nullptr) // Check if the list is empty
             return nullptr;
 
-        int length = getLength(head);
-        int mid = length / 2;
-        ListNode* temp = head;
-        int cnt = 0;
-        while (cnt < mid) {
-            temp = temp->next;
-            cnt++;
+    //     int length = getLength(head);
+    //     int mid = length / 2;
+    //     ListNode* temp = head;
+    //     int cnt = 0;
+    //     while (cnt < mid) {
+    //         temp = temp->next;
+    //         cnt++;
+    //     }
+    //     return temp; // Return the middle node
+        ListNode *slow = head;
+        ListNode *fast = head;
+        while (fast!=NULL && fast->next!=NULL){
+        slow = slow->next;
+        fast = fast->next->next;
         }
-        return temp; // Return the middle node
+        return slow;
     }
 };
