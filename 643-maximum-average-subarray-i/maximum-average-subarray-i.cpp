7b8e2198a84bdc963e43130ec5bafbe89bea3a03
Ayushwe <114506169+Ayushwe@users.noreply.github.com>
2025-01-28 15:19:14 +0530

    Time: 0 ms (100.00%) | Memory: 113.8 MB (46.32%) - LeetSync

--- 643-maximum-average-subarray-i/maximum-average-subarray-i.cpp
+++ 643-maximum-average-subarray-i/maximum-average-subarray-i.cpp
@@ -1,28 +1,27 @@
 class Solution {
 public:
     double findMaxAverage(vector<int>& nums, int k) {
-         // Edge case: If nums size is less than k, return the average of the entire array
-        if (nums.size() < k) {
-            double totalSum = 0;
-            for (int num : nums) {
-                totalSum += num;
-            }
-            return totalSum / nums.size();
+        // get the sum of Starting Window
+        int sum = 0;
+        int maxSum = 0;
+        for(int i=0;i<k;i++){
+            sum += nums[i];
         }
-        
-        // Calculate prefix sums
-        vector<double> prefix(nums.size(), 0);
-        prefix[0] = nums[0];
-        for (int i = 1; i < nums.size(); i++) {
-            prefix[i] = prefix[i - 1] + nums[i];
+        maxSum = sum;
+        // start sliding window
+        int StartIndex = 0;
+        int lastIndex = k;
+        while(lastIndex < nums.size()){
+            // remove previous element 
+            sum -= nums[StartIndex];
+            StartIndex++;
+            // add next element
+            sum += nums[lastIndex];
+            lastIndex++;
+            // update max sum
+            maxSum = max(sum,maxSum);
         }
-        
-        double ans = prefix[k - 1] / k;  // First subarray of length k
-        for (int i = k; i < nums.size(); i++) {
-            double subarraySum = prefix[i] - prefix[i - k];
-            ans = max(ans, subarraySum / k);
-        }
-        
+        double ans = maxSum/(double)k;
         return ans;
     }
 };
