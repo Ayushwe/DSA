b6c7120c262415f609ab1d48577f1dd7141060c9
Ayushwe <114506169+Ayushwe@users.noreply.github.com>
2025-02-11 13:36:16 +0530

    Time: 1 ms (56.95%) | Memory: 9.4 MB (27.69%) - LeetSync

--- 205-isomorphic-strings/isomorphic-strings.cpp
+++ 205-isomorphic-strings/isomorphic-strings.cpp
@@ -5,6 +5,7 @@ public:
             return false;
         }
         unordered_map<char,char>MapST;
+        unordered_map<char,char>MapTS;
         for(int i=0;i<s.length();i++){
             char c1 = s[i];
             char c2 = t[i];
@@ -12,13 +13,11 @@ public:
             if(MapST.count(c1) && MapST[c1] != c2){
                 return false;
             }
-
-            for(auto& [key,value] : MapST){
-                if(value==c2 && key!=c1){
-                    return false;
-                }
+            if(MapTS.count(c2) && MapTS[c2] != c1){
+                return false;
             }
             MapST[c1]=c2;
+            MapTS[c2]=c1;
         }
         return true;
     }
