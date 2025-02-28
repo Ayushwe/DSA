d78705072288e559e33250199fcb1c9162300e26
Ayushwe <114506169+Ayushwe@users.noreply.github.com>
2025-02-28 19:16:23 +0530

    Time: 0 ms (100.00%) | Memory: 44.6 MB (51.18%) - LeetSync

--- 142-linked-list-cycle-ii/linked-list-cycle-ii.java
+++ 142-linked-list-cycle-ii/linked-list-cycle-ii.java
@@ -1,32 +1,32 @@
 /**
  * Definition for singly-linked list.
  * class ListNode {
- *     int val;
- *     ListNode next;
- *     ListNode(int x) {
- *         val = x;
- *         next = null;
- *     }
+ * int val;
+ * ListNode next;
+ * ListNode(int x) {
+ * val = x;
+ * next = null;
+ * }
  * }
  */
 public class Solution {
-
-    ListNode findStart(ListNode head,ListNode fast,ListNode slow){
+    ListNode FindStart(ListNode head, ListNode slow, ListNode fast) {
         slow = head;
-        while(slow != fast){
+        while (slow != fast) {
             slow = slow.next;
             fast = fast.next;
         }
         return fast;
     }
+
     public ListNode detectCycle(ListNode head) {
         ListNode slow = head;
         ListNode fast = head;
-        while(fast != null && fast.next != null){
+        while (fast != null && fast.next != null) {
             slow = slow.next;
             fast = fast.next.next;
-            if(fast == slow){
-                return findStart(head,fast,slow);
+            if (fast == slow) {
+                return FindStart(head, slow, fast);
             }
         }
         return null;
