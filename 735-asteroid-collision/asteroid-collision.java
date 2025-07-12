1c02c4cfa7c6218f33b47c67aaf6d98bfa2f2ece
Ayushwe <114506169+Ayushwe@users.noreply.github.com>
2025-07-12 21:10:22 +0530

    Time: 5 ms (84.84%) | Memory: 45.6 MB (34.07%) - LeetSync

--- 735-asteroid-collision/asteroid-collision.java
+++ 735-asteroid-collision/asteroid-collision.java
@@ -1,32 +1,32 @@
-import java.util.Stack;
-
 class Solution {
     public int[] asteroidCollision(int[] asteroids) {
-        int n = asteroids.length;
         Stack<Integer> st = new Stack<>();
-        
-        for (int i = 0; i < n; i++) {
+        for (int i = 0; i < asteroids.length; i++) {
             if (st.isEmpty() || asteroids[i] > 0) {
                 st.push(asteroids[i]);
             } else {
-                while (!st.isEmpty() && st.peek() > 0) {
-                    if (Math.abs(st.peek()) < Math.abs(asteroids[i])) {
-                        st.pop();
-                        continue;
-                    } else if (Math.abs(st.peek()) == Math.abs(asteroids[i])) {
-                        st.pop();
-                        // Prevents pushing the negative asteroid
-                        asteroids[i] = 0;
+                while (!st.isEmpty()) {
+                    if (st.peek() < 0) {
+                        st.push(asteroids[i]);
+                        break;
                     }
-                    break;
-                }
-                // Only push if the asteroid was not destroyed (not `0`)
-                if (asteroids[i] != 0 && (st.isEmpty() || st.peek() < 0)) {
-                    st.push(asteroids[i]);
+                    if(Math.abs(asteroids[i]) == st.peek()){
+                        st.pop();
+                        break;
+                    }
+                    else if(Math.abs(asteroids[i]) < st.peek()){
+                        break;
+                    }
+                    else{
+                        st.pop();
+                        if(st.isEmpty()){
+                            st.push(asteroids[i]);
+                            break;
+                        }
+                    }   
                 }
             }
         }
-
         int[] ans = new int[st.size()];
         for (int i = ans.length - 1; i >= 0; i--) {
             ans[i] = st.pop();
