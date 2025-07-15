29b6f5d539908c17305897902aba264f0aa2dba6
Ayushwe <114506169+Ayushwe@users.noreply.github.com>
2025-07-15 14:13:04 +0530

    Time: 5 ms (35.06%) | Memory: 45.1 MB (44.13%) - LeetSync

--- 345-reverse-vowels-of-a-string/reverse-vowels-of-a-string.java
+++ 345-reverse-vowels-of-a-string/reverse-vowels-of-a-string.java
@@ -1,20 +1,18 @@
 class Solution {
     public String reverseVowels(String s) {
-        ArrayList<Character>temp = new ArrayList<>();
+        ArrayList<Character>vowels = new ArrayList<>();
         for(char ch : s.toCharArray()){
-            if(ch == 'A' || ch == 'a' || ch == 'E' || ch == 'e' || ch == 'I' || ch == 'i' || ch == 'O' || ch == 'o' || ch == 'U' || ch == 'u' ){
-                temp.add(ch);
+            if(ch == 'A'||ch == 'a'||ch == 'E'||ch == 'e'||ch == 'I'||ch == 'i'||ch == 'O'||ch == 'o'||ch == 'U'||ch == 'u'){
+                vowels.add(ch);
             }
         }
-        Collections.reverse(temp);
+        Collections.reverse(vowels);
+        int index = 0;
         StringBuilder ans = new StringBuilder();
-
-        int j=0;
-        for(int i=0;i<s.length();i++){
-            char ch = s.charAt(i);
-            if(ch == 'A' || ch == 'a' || ch == 'E' || ch == 'e' || ch == 'I' || ch == 'i' || ch == 'O' || ch == 'o' || ch == 'U' || ch == 'u' ){
-                ans.append(temp.get(j));
-                j++;
+        for(char ch : s.toCharArray()){
+            if(ch == 'A'||ch == 'a'||ch == 'E'||ch == 'e'||ch == 'I'||ch == 'i'||ch == 'O'||ch == 'o'||ch == 'U'||ch == 'u'){
+                ans.append(vowels.get(index));
+                index++;
             }
             else{
                 ans.append(ch);
