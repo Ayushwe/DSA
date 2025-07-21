5dc8d612dc18601a10f97de127af7f6c3a5eda17
Ayushwe <114506169+Ayushwe@users.noreply.github.com>
2025-07-21 10:01:26 +0530

    Time: 1 ms (63.48%) | Memory: 41.3 MB (88.07%) - LeetSync

--- 14-longest-common-prefix/longest-common-prefix.java
+++ 14-longest-common-prefix/longest-common-prefix.java
@@ -3,14 +3,14 @@ class Solution {
         Arrays.sort(strs);
         String first = strs[0];
         String last = strs[strs.length-1];
-        int i=0;
-        int j=0;
-        StringBuilder ans = new StringBuilder();
+        int i = 0;
+        int j = 0;
+        StringBuilder ans = new StringBuilder(); 
         while(i<first.length() && j<last.length()){
             if(first.charAt(i) != last.charAt(j)){
                 break;
             }
-            else{
+            else if(first.charAt(i) == last.charAt(j)){
                 ans.append(first.charAt(i));
                 i++;
                 j++;
