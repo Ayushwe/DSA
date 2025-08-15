0219dda93e071a1d098d533be1e174055234887e
Ayushwe <114506169+Ayushwe@users.noreply.github.com>
2025-08-15 14:32:22 +0530

    Time: 16 ms (21.85%) | Memory: 45.2 MB (6.71%) - LeetSync

--- 242-valid-anagram/valid-anagram.java
+++ 242-valid-anagram/valid-anagram.java
@@ -9,12 +9,18 @@ class Solution {
         }
         for(char ch : t.toCharArray()){
             map.put(ch,map.getOrDefault(ch,0)-1);
-        }
-        for(Map.Entry<Character,Integer>entry : map.entrySet()){
-            if(entry.getValue() != 0){
-                return false;
+            if(map.get(ch) == 0){
+                map.remove(ch);
             }
         }
-        return true;
+        // for(Map.Entry<Character,Integer>entry : map.entrySet()){
+        //     if(entry.getValue() != 0){
+        //         return false;
+        //     }
+        // }
+        if(map.size()==0){
+            return true;
+        }
+        return false;
     }
 }
